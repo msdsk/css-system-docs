4bcdcc9487777627f84485863ef9c35f831e7c42
Michal Sadowski <saski.michal.sadowski@gmail.com>
2019-12-16 13:05:31 +0100

    Load Open Sans

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,700&display=swap' }
     ]
   },
   /*
