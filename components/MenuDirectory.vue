693a7d491f06e56972e1d1e08f1e29fac738e6cd
Michal Sadowski <saski.michal.sadowski@gmail.com>
2019-12-16 12:17:15 +0100

    Add hot swap css ability

--- components/MenuDirectory.vue
+++ components/MenuDirectory.vue
@@ -1,6 +1,6 @@
 <template>
-  <ul>
-    <li v-for="file in directory.files" :key="file">
+  <ul class="menu-directory">
+    <li v-for="file in directory.files" :key="file" class="menu-directory__item">
       <a :href="file">{{formattedFileName(file)}}</a>
     </li>
     <li
@@ -27,5 +27,12 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+.menu-directory {
+  list-style-type: none;
+
+  .menu-directory {
+    margin-left: 1em;
+  }
+}
 </style>
--- server_lib/cssCopier.js
+++ server_lib/cssCopier.js
@@ -1,5 +1,6 @@
 const fs = require("fs-extra")
 const RecursiveRename = require('recursive-rename')
+const watch = require('node-watch')
 
 module.exports = async (modulePath, dest) => {
   fs.removeSync(dest)
@@ -10,4 +11,11 @@ module.exports = async (modulePath, dest) => {
     path: dest
   })
   await recursiveRename.dive()
+  if (process.env.NODE_ENV === 'production') return
+
+  watch(modulePath, { recursive: true, filter: /\.scss$/ }, (e, name) => {
+    const fileDestination = name.replace(modulePath, dest)
+    fs.copySync(name, fileDestination)
+    fs.renameSync(fileDestination, fileDestination.replace(/\.scss$/, '.scss-doc'))
+  })
 }
